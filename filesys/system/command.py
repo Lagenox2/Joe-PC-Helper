e4f90aeb9b708a3d7a104a5693a78e5dadef6bd2
Lagenox2 <lagenox2@gmail.com>
2024-08-17 19:35:14 +0300

    title cmd

--- filesys/system/command.py
+++ filesys/system/command.py
@@ -10,6 +10,6 @@ from threading import Thread
 
 def command():
     command_win = Tk()
-    command_win.title('2024 Operating System. All right reserved.')
+    command_win.title('2024 Operating System. All right reserved. Command Prompt')
     command_win.geometry('950x514')
     command_win.mainloop()
